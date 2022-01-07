653ff27858a53211d91d5783febb9feb0b066bd2
vickyruud <aprajit.k@gmail.com>
2022-01-07 13:02:01 -0500

    refactor and add helpers file

--- game.rb
+++ game.rb
@@ -1,5 +1,7 @@
 require './player.rb'
 require './questions.rb'
+require './helpers.rb'
+
 
 class Game
   attr_accessor :player1, :player2, :question
@@ -8,41 +10,7 @@ class Game
     @player2 = Player.new
     @current_player = @player1
   end
-
-  def game_loop
-    @questionObj = Question.new
-    @question = @questionObj.create_question
-    puts "#{@current_player.name}: #{@question[0]}"
-    @input = gets.chomp.to_i
-    if @input == @question[1]
-      puts "YES! You are correct."
-    else
-      @current_player.reduce_lives
-      puts "Seriously? No!!"
-    end
-    puts "P1: #{@player1.lives}/3 vs P2: #{@player2.lives}/3"
-
-    if @player1.lives > 0 && @player2.lives > 0
-      puts "----- NEW TURN -----" 
-      toggle_player
-    else
-      if @player1.lives > @player2.lives
-        puts "#{@player1.name} wins with a score of #{@player1.lives}/3"
-      else
-        puts "#{@player2.name} wins with a score of #{@player2.lives}/3"
-      end
-      puts "----Game Over----"
-    end    
-  end
   
-  def toggle_player 
-    if @current_player == @player1
-      @current_player = @player2
-    else
-      @current_player = @player1
-    end
-  end
-
   def run_game
     while @player1.lives > 0 && @player2.lives > 0
       game_loop
---
+++ helpers.rb
@@ -0,0 +1,36 @@
+##Changes player each turn
+def toggle_player 
+    if @current_player == @player1
+      @current_player = @player2
+    else
+      @current_player = @player1
+    end
+end
+
+
+##runs the game loop
+def game_loop
+    @questionObj = Question.new
+    @question = @questionObj.create_question
+    puts "#{@current_player.name}: #{@question[0]}"
+    @input = gets.chomp.to_i
+    if @input == @question[1]
+      puts "YES! You are correct."
+    else
+      @current_player.reduce_lives
+      puts "Seriously? No!!"
+    end
+    puts "P1: #{@player1.lives}/3 vs P2: #{@player2.lives}/3"
+
+    if @player1.lives > 0 && @player2.lives > 0
+      puts "----- NEW TURN -----" 
+      toggle_player
+    else
+      if @player1.lives > @player2.lives
+        puts "#{@player1.name} wins with a score of #{@player1.lives}/3"
+      else
+        puts "#{@player2.name} wins with a score of #{@player2.lives}/3"
+      end
+      puts "----Game Over----"
+    end    
+end
